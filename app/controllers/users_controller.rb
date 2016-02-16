70998f2b5e5099c4f901c986eab4a428a9330533
zibs <elizibin@gmail.com>
2016-02-15 17:46:33 -0800

    Refactor views into partials. Add Edit/update action for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update, :show, :destroy]
 
   def new
     @user = User.new
@@ -16,12 +17,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
 
+  def update
+    if @user.update(user_params)
+      redirect_to root_path, flash: {success: "Information Renewed..."}
+    else
+      render :edit
+    end
+  end
 
+  def destroy
+  end
 
     private
+
       def user_params
         params.require(:user).permit([:first_name, :last_name, :email, :password, :password_confirmation])
+      end
+
+      def find_user
+        @user = User.find(params[:id])
       end
 
 end
