587934cea90bce2c8dc0f2e07db6d3e865a2b7e1
zibs <elizibin@gmail.com>
2016-02-19 15:48:16 -0800

    Add Background job. Add Rake Task. The background job was generated by brails g job job_class_name The rake task was generated by brails g task task_name

--- app/jobs/send_admin_report_job.rb
+++ app/jobs/send_admin_report_job.rb
@@ -1,7 +1,13 @@
 class SendAdminReportJob < ActiveJob::Base
   queue_as :default
-
+# arguments will be an array
   def perform(*args)
     # Do something later
+
+
   end
 end
+# SendAdminReportJob.perform_now
+# SendAdminReportJob.perform_later
+
+# ^^ will call perform internally
---
+++ lib/tasks/send_reports.rake
@@ -0,0 +1,9 @@
+namespace :send_reports do
+  # without environment it will just run plain ruby.
+
+  desc "Send All Reports in the System"
+  task :send_all => :environment do
+    puts Cowsay.say("Sending Reports")
+    # SendAdminReportJob.perform_later()
+  end
+end
