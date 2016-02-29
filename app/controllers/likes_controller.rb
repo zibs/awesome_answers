3fd64cf8b379ae3c32efa31a697a4becc9ee1964
zibs <elizibin@gmail.com>
2016-02-29 15:12:38 -0800

    Ajaxify Liking and Unliking.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,29 @@
 class LikesController < ApplicationController
   before_action :authenticate_user
-  
 
   def create
-    question = Question.find(params[:question_id])
-    like = Like.new(question: question, user: current_user)
-    if like.save
-    # current_user.liked_questions << question
-      redirect_to question, flash: { success: "Liked" }
-    else
-    redirect_to question, flash: { info: "Already Liked"}
+    @question = Question.find(params[:question_id])
+    like = Like.new(question: @question, user: current_user)
+    respond_to do |format|
+      if like.save
+      # current_user.liked_questions << question
+        format.html {redirect_to @question, flash: { success: "Liked" } }
+        format.js { render :successfully_liked }
+      else
+        format.html { redirect_to @question, flash: { info: "Already  Liked"} }
+        format.js { render :unsuccessfully_liked }
+      end
     end
   end
 
   def destroy
-    like = current_user.likes.find(params[:id])
-    like.destroy
-    redirect_to question_path(params[:question_id]), flash: { warning: "Unliked"}
+    @like = current_user.likes.find(params[:id])
+    @question = Question.find params[:question_id]
+    @like.destroy
+    respond_to do |format|
+      format.html { redirect_to question_path(params[:question_id]), flash: { warning: "Unliked"} }
+      format.js { render }
+    end
   end
-
 
 end
