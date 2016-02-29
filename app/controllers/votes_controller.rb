9c2b4110fe65d2e1e06ca97bc8e2fc7b4f2b6d10
zibs <elizibin@gmail.com>
2016-02-29 15:41:41 -0800

    Ajaxify Voting. Ajax create, update, and destroy actions in Voting Controller. Add partials in Votes Views, and render across views.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,33 +2,45 @@ class VotesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    question = Question.find(params[:question_id])
+    @question = Question.find(params[:question_id])
     vote = Vote.new(vote_params)
     vote.user = current_user
-    vote.question = question
+    vote.question = @question
     # flash = (vote.save) ? { notice: "Voted!" } : { alert: "Try Again!" }
     # redirect_to question_path(question), flash
-    if vote.save
-      redirect_to question_path(question), flash: { success: "Voted!"}
-    else
-      redirect_to question_path(question), flash: {warning: "Try Again"}
+    respond_to do |format|
+      if vote.save
+        format.html { redirect_to question_path(@question), flash: { success: "Voted!"} }
+        format.js { render :successful_vote }
+      else
+        format.html { redirect_to question_path(@question), flash: {warning: "Try Again"} }
+        format.js { render :unsuccessful_vote }
+      end
     end
   end
 
   def update
-    question = Question.find(params[:question_id])
+    @question = Question.find(params[:question_id])
     vote     = current_user.votes.find(params[:id])
-    vote.update(vote_params)
-    redirect_to question_path(question), flash: { info: "Updated!"}
-    # else
-    #   redirect_to question_path(question), flash: { info: "Try again!"}
-    # end
+    respond_to do |format|
+      if vote.update(vote_params)
+        format.html {redirect_to question_path(@question), flash: { info: "Updated!"} }
+        format.js { render :successful_vote_update }
+      else
+        format.html { redirect_to question_path(@question), flash: { info: "try again!"} }
+        format.js { render :unsuccessful_vote_update }
+      end
+    end
   end
 
   def destroy
     vote = current_user.votes.find(params[:id])
+    @question = Question.find(params[:question_id])
     vote.destroy
-    redirect_to question_path(params[:question_id]), flash: {danger: "Unvoted"}
+    respond_to do |format|
+      format.html { redirect_to question_path(params[:question_id]), flash: {danger: "Unvoted"} }
+      format.js { render }
+    end
   end
 
     private
