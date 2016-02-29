35ef1e5e37331247b241f2e44705e618e6864b8e
zibs <elizibin@gmail.com>
2016-02-29 15:23:20 -0800

    Ajaxify Favouriting. Refactor create and destory actions in Favourites Control to use respond to Ajax requests. Add partials for rendering js and views.

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -6,19 +6,27 @@ class FavouritesController < ApplicationController
   end
 
   def create
-    question = Question.find(params[:question_id])
-    favourite = Favourite.new(question: question, user: current_user)
-    if favourite.save
-      redirect_to question, flash: { success: "<3"}
-    else
-      redirect_to question, flash: { info: "already <3ed"}
+    @question = Question.find(params[:question_id])
+    favourite = Favourite.new(question: @question, user: current_user)
+    respond_to do |format|
+      if favourite.save
+        format.html { redirect_to @question, flash: { success: "<3"} }
+        format.js { render :successful_favourite }
+      else
+        format.html { redirect_to @question, flash: { info: "already <3ed"} }
+        format.js { render :unsuccessful_favourite }
+      end
     end
   end
 
   def destroy
     favourite = current_user.favourites.find(params[:id])
+    @question = Question.find(params[:question_id])
     favourite.destroy
-    redirect_to question_path(params[:question_id]), flash: {warning: "un<3ed"}
+    respond_to do |format|
+      format.html { redirect_to question_path(params[:question_id]), flash: {warning: "un<3ed"} }
+      format.js { render }
+    end
   end
 
 end
